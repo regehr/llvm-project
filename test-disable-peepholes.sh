e9e0c8e2d9f3bcdf8ca2594b862ecab6ce70a64c
John Regehr <regehr@cs.utah.edu>
2020-06-08 12:34:13 -0600

    foo

---
+++ test-disable-peepholes.sh
@@ -0,0 +1,75 @@
+set -e
+
+export DIR=$HOME/llvm-project
+export CMAKE='cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON ../llvm -DLLVM_ENABLE_PROJECTS=clang;openmp'
+
+rm -rf $DIR/build-*
+
+echo "step 1"
+
+(
+set -e
+mkdir $DIR/build-peeps
+cd $DIR/build-peeps
+$CMAKE -DCMAKE_INSTALL_PREFIX=$DIR/install-peeps > cmake.out 2>&1
+ninja > build.out 2>&1
+# only fails if LLVM is broken
+ninja check > check.out 2>&1
+ninja install
+)
+
+echo "step 2"
+
+(
+set -e
+export PATH=$DIR/install-peeps/bin:$PATH
+mkdir $DIR/build-peeps2
+cd $DIR/build-peeps2
+$CMAKE -DCMAKE_INSTALL_PREFIX=$DIR/install-peeps2 > cmake.out 2>&1
+ninja > build.out 2>&1
+# only fails if LLVM is broken
+ninja check > check.out 2>&1
+ninja install
+)
+
+echo "step 3"
+
+# dodgy part, yikes!
+cd $DIR
+perl -pi.bak -e "s/bool DisablePeepholes = false;/bool DisablePeepholes = true;/" llvm/lib/Analysis/InstructionSimplify.cpp
+
+(
+set -e
+mkdir $DIR/build-no-peeps
+cd $DIR/build-no-peeps
+$CMAKE -DCMAKE_INSTALL_PREFIX=$DIR/install-no-peeps > cmake.out 2>&1
+ninja > build.out 2>&1
+# don't test this one! it fails on a huge number of tests, by design
+ninja install
+)
+
+echo "step 4"
+
+# the other dodgy part, yikes!
+cd $DIR
+perl -pi.bak -e "s/bool DisablePeepholes = true;/bool DisablePeepholes = false;/" llvm/lib/Analysis/InstructionSimplify.cpp
+
+(
+set -e
+export PATH=$DIR/install-no-peeps/bin:$PATH
+mkdir $DIR/build-no-peeps2
+cd $DIR/build-no-peeps2
+$CMAKE -DCMAKE_INSTALL_PREFIX=$DIR/install-no-peeps2 > cmake.out 2>&1
+ninja > build.out 2>&1
+# this is the risky one!
+ninja check > check.out 2>&1
+ninja install
+)
+
+echo "There should be no diffs here!"
+
+cd $DIR
+git diff
+
+size $DIR/build-peeps2/bin/clang
+size $DIR/build-no-peeps2/bin/clang
