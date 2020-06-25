e733506ad3fd34eb8f7ae8a455f03cf25796bc37
John Regehr <regehr@cs.utah.edu>
2020-06-25 08:34:20 -0600

    foo

--- test-disable-peepholes.sh
+++ test-disable-peepholes.sh
@@ -5,11 +5,24 @@ export CMAKE='cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G Nin
 
 rm -rf $DIR/build-*
 
+# this LLVM should behave identically to its twin on the main branch
 (
-echo "step 0"
 set -e
 mkdir $DIR/build-default
 cd $DIR/build-default
+$CMAKE -DCMAKE_INSTALL_PREFIX=$DIR/install-default -DCMAKE_CXX_FLAGS='-DDISABLE_WRONG_OPTIMIZATIONS_DEFAULT_VALUE=false -DDISABLE_PEEPHOLES_DEFAULT_VALUE=false' > cmake.out 2>&1
+ninja > build.out 2>&1
+# only fails if LLVM is broken
+ninja check > check.out 2>&1
+ninja install
+)
+
+# this makes sure our default LLVM can build a working LLVM
+(
+set -e
+export PATH=$DIR/install-default/bin:$PATH
+mkdir $DIR/build-default2
+cd $DIR/build-default2
 $CMAKE -DCMAKE_INSTALL_PREFIX=$DIR/install-peeps -DCMAKE_CXX_FLAGS='-DDISABLE_WRONG_OPTIMIZATIONS_DEFAULT_VALUE=false -DDISABLE_PEEPHOLES_DEFAULT_VALUE=false' > cmake.out 2>&1
 ninja > build.out 2>&1
 # only fails if LLVM is broken
@@ -18,18 +31,6 @@ ninja install
 )
 
 (
-echo "step 1"
-set -e
-mkdir $DIR/build-peeps
-cd $DIR/build-peeps
-$CMAKE -DCMAKE_INSTALL_PREFIX=$DIR/install-peeps -DCMAKE_CXX_FLAGS='-DDISABLE_WRONG_OPTIMIZATIONS_DEFAULT_VALUE=true -DDISABLE_PEEPHOLES_DEFAULT_VALUE=false' > cmake.out 2>&1
-ninja > build.out 2>&1
-# ninja check > check.out 2>&1
-ninja install
-)
-
-(
-echo "step 2"
 set -e
 export PATH=$DIR/install-peeps/bin:$PATH
 mkdir $DIR/build-peeps2
@@ -42,7 +43,6 @@ ninja install
 )
 
 (
-echo "step 3"
 set -e
 mkdir $DIR/build-no-peeps
 cd $DIR/build-no-peeps
@@ -52,12 +52,7 @@ ninja > build.out 2>&1
 ninja install
 )
 
-# the other dodgy part, yikes!
-cd $DIR
-perl -pi.bak -e "s/bool DisablePeepholes = true;/bool DisablePeepholes = false;/" llvm/lib/Analysis/InstructionSimplify.cpp
-
 (
-echo "step 4"
 set -e
 export PATH=$DIR/install-no-peeps/bin:$PATH
 mkdir $DIR/build-no-peeps2
@@ -68,11 +63,6 @@ ninja > build.out 2>&1
 ninja check > check.out 2>&1
 ninja install
 )
-
-echo "There should be no diffs here!"
-
-cd $DIR
-git diff
 
 size $DIR/build-peeps2/bin/clang
 size $DIR/build-no-peeps2/bin/clang
