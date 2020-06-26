6cc05f8ffb38dd6d2cbd9a69792a1d132c57afe4
John Regehr <regehr@cs.utah.edu>
2020-06-26 10:32:15 -0600

    foo

--- souper-scripts/test-spec.sh
+++ souper-scripts/test-spec.sh
@@ -1,15 +1,23 @@
+set -e
+
 export SPEC=$HOME/cpu2017
 export DIR=$HOME/llvm-project
 export ARGS="--rebuild --input test 600.perlbench_s 602.gcc_s 605.mcf_s 620.omnetpp_s 623.xalancbmk_s 625.x264_s 631.deepsjeng_s 641.leela_s 657.xz_s"
 
-
-/home/regehr/llvm-project/install-default2
-/home/regehr/llvm-project/install-no-ub2
-/home/regehr/llvm-project/install-no-ub-no-peeps2
-
-cp $DIR/souper-scripts/default.cfg $SPEC/config
-
 cd $SPEC
 . ./shrc
 
+sed -e 's/LLVMDIR/\/home\/regehr\/llvm-project\/install-default2/g' < $DIR/souper-scripts/default.cfg > $SPEC/config/default.cfg
 runcpu $ARGS
+
+sed -e 's/LLVMDIR/\/home\/regehr\/llvm-project\/install-no-ub2/g' < $DIR/souper-scripts/default.cfg > $SPEC/config/default.cfg
+runcpu $ARGS
+
+sed -e 's/LLVMDIR/\/home\/regehr\/llvm-project\/install-no-ub-no-peeps2/g' < $DIR/souper-scripts/default.cfg > $SPEC/config/default.cfg
+runcpu $ARGS
+
+echo
+echo =======
+echo SUCCESS
+echo =======
+echo
