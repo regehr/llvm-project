524bbf103f9fc13fbe205f04fba43e38ab9fb541
John Regehr <regehr@cs.utah.edu>
2021-05-17 10:31:41 -0600

    foo

--- llvm/tools/llvm-reduce/deltas/ReduceOperands.cpp
+++ llvm/tools/llvm-reduce/deltas/ReduceOperands.cpp
@@ -22,131 +22,48 @@
 
 using namespace llvm;
 
-/// Goes over OldF calls and replaces them with a call to NewF
-static void replaceFunctionCalls(Function &OldF, Function &NewF,
-                                 const std::set<int> &ArgIndexesToKeep) {
-  const auto &Users = OldF.users();
-  for (auto I = Users.begin(), E = Users.end(); I != E; )
-    if (auto *CI = dyn_cast<CallInst>(*I++)) {
-      SmallVector<Value *, 8> Args;
-      for (auto ArgI = CI->arg_begin(), E = CI->arg_end(); ArgI != E; ++ArgI)
-        if (ArgIndexesToKeep.count(ArgI - CI->arg_begin()))
-          Args.push_back(*ArgI);
-
-      CallInst *NewCI = CallInst::Create(&NewF, Args);
-      NewCI->setCallingConv(NewF.getCallingConv());
-      if (!CI->use_empty())
-        CI->replaceAllUsesWith(NewCI);
-      ReplaceInstWithInst(CI, NewCI);
-    }
+static Value *getDefaultValue(Type *T) {
+  return 0;
 }
 
 /// Removes out-of-chunk arguments from functions, and modifies their calls
 /// accordingly. It also removes allocations of out-of-chunk arguments.
-static void instToArgumentInModule(std::vector<Chunk> ChunksToKeep,
+static void reduceOperandsInModule(std::vector<Chunk> ChunksToKeep,
                                    Module *Program) {
   Oracle O(ChunksToKeep);
 
-  std::vector<Function *> Funcs;
-  for (auto &F : *Program)
-    Funcs.push_back(&F);
-
-  for (auto F : Funcs) {
-    llvm::outs() << "function: " << F->getName() << "\n";
+  for (auto &F : *Program) {
+    llvm::outs() << "function: " << F.getName() << "\n";
     
-    // Make a list of instructions in the current function that are in
-    // the chunk and that do not return void
-    std::vector<Instruction *> InstToDelete;
-    for (auto &BB : *F) {
-      int i = 0;
-      for (auto &Inst : BB) {
-        llvm::outs() << i++ << " : ";
-        bool k = O.shouldKeep();
-        if (k)
-          llvm::outs() << "keep\n";
-        else
-          llvm::outs() << "remove\n";
-        if (!k && !Inst.getType()->isVoidTy())
-          InstToDelete.push_back(&Inst);
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        int NumOperands = I.getNumOperands();
+        for (int OpIndex = 0; OpIndex < NumOperands; ++OpIndex) {
+          if (O.shouldKeep())
+            continue;
+          I.setOperand(OpIndex, getDefaultValue(I.getOperand(OpIndex)->getType()));
+        }
       }
     }
-
-    // Bail early if we're not changing this function
-    if (InstToDelete.empty())
-      continue;
-
-    // Start with original argument list
-    std::vector<Type *> ArgTy;
-    for (auto &A : F->args())
-      ArgTy.push_back(A.getType());
-
-    // Add an argument corresponding to value produced by each deleted
-    // insn
-    for (auto &Inst : InstToDelete)
-      ArgTy.push_back(Inst->getType());
-    
-    auto FuncTy = FunctionType::get(F->getReturnType(), ArgTy, F->isVarArg());
-    auto ClonedFunc = Function::Create(FuncTy, F->getLinkage(), F->getAddressSpace(),
-                                       F->getName(), Program);
-
-    ValueToValueMapTy VMap;
-    auto A = ClonedFunc->arg_begin();
-    for (auto &V : F->args())
-      VMap[&V] = A++;
-    for (auto &Inst : InstToDelete)
-      VMap[Inst] = A++;
-
-    // Delete any (unique) instruction that uses the argument
-    for (Value *V : InstToDelete) {
-      auto *I = cast<Instruction>(V);
-      I->replaceAllUsesWith(UndefValue::get(I->getType()));
-      if (!I->isTerminator())
-        I->eraseFromParent();
-    }
-
-#if 0
-    std::set<int> ArgIndexesToKeep;
-    for (auto &Arg : enumerate(F->args()))
-      if (ArgsToKeep.count(&Arg.value()))
-        ArgIndexesToKeep.insert(Arg.index());
-#endif
-
-    SmallVector<ReturnInst *, 8> Returns;
-    CloneFunctionInto(ClonedFunc, F, VMap,
-                      CloneFunctionChangeType::LocalChangesOnly, Returns);
-      
-    // In order to preserve function order, we move Clone after old Function
-    ClonedFunc->removeFromParent();
-    Program->getFunctionList().insertAfter(F->getIterator(), ClonedFunc);
-
-#if 0
-    // FIXME!!!! must update calls to have extra undef arguments of the correct types
-    replaceFunctionCalls(F, *ClonedFunc, ArgIndexesToKeep);
-#endif
-
-    // Rename Cloned Function to Old's name
-    std::string FName = std::string(F->getName());
-    F->replaceAllUsesWith(ConstantExpr::getBitCast(ClonedFunc, F->getType()));
-    F->eraseFromParent();
-    ClonedFunc->setName(FName);
   }
 }  
 
 /// Counts the amount of basic blocks and prints their name & respective index
-static unsigned countInstructions(Module *Program) {
+static unsigned countOperands(Module *Program) {
   // TODO: Silence index with --quiet flag
   outs() << "----------------------------\n";
-  int InstCount = 0;
+  int OperandCount = 0;
   for (auto &F : *Program)
     for (auto &BB : F)
-      InstCount += BB.getInstList().size();
-  outs() << "Number of instructions: " << InstCount << "\n";
+      for (auto &I : BB)
+        OperandCount += I.getNumOperands();
+  outs() << "Number of operands: " << OperandCount << "\n";
 
-  return InstCount;
+  return OperandCount;
 }
 
 void llvm::reduceOperandsDeltaPass(TestRunner &Test) {
-  outs() << "*** Reducing Instructions to Arguments...\n";
-  unsigned InstCount = countInstructions(Test.getProgram());
-  runDeltaPass(Test, InstCount, instToArgumentInModule);
+  outs() << "*** Reducing Operands...\n";
+  unsigned OperandCount = countOperands(Test.getProgram());
+  runDeltaPass(Test, OperandCount, reduceOperandsInModule);
 }
