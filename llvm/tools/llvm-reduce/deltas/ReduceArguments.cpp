3025bf09af5523fea91121bbc21c045d322db708
John Regehr <regehr@cs.utah.edu>
2023-06-25 09:52:01 -0600

    foo

--- llvm/tools/llvm-reduce/deltas/ReduceArguments.cpp
+++ llvm/tools/llvm-reduce/deltas/ReduceArguments.cpp
@@ -24,99 +24,32 @@
 
 using namespace llvm;
 
-/// Goes over OldF calls and replaces them with a call to NewF
-static void replaceFunctionCalls(Function &OldF, Function &NewF,
-                                 const std::set<int> &ArgIndexesToKeep) {
-  const auto &Users = OldF.users();
-  for (auto I = Users.begin(), E = Users.end(); I != E; )
-    if (auto *CI = dyn_cast<CallInst>(*I++)) {
-      // Skip uses in call instructions where OldF isn't the called function
-      // (e.g. if OldF is an argument of the call).
-      if (CI->getCalledFunction() != &OldF)
-        continue;
-      SmallVector<Value *, 8> Args;
-      for (auto ArgI = CI->arg_begin(), E = CI->arg_end(); ArgI != E; ++ArgI)
-        if (ArgIndexesToKeep.count(ArgI - CI->arg_begin()))
-          Args.push_back(*ArgI);
-
-      CallInst *NewCI = CallInst::Create(&NewF, Args);
-      NewCI->setCallingConv(NewF.getCallingConv());
-      if (!CI->use_empty())
-        CI->replaceAllUsesWith(NewCI);
-      ReplaceInstWithInst(CI, NewCI);
-    }
+/// FIXME: return false for functions that have a call in this module,
+/// or are address taken
+static bool shouldRemoveReturns(const Function &F) {
+  return !F.isDeclaration();
 }
 
-/// Returns whether or not this function should be considered a candidate for
-/// argument removal. Currently, functions with no arguments and intrinsics are
-/// not considered. Intrinsics aren't considered because their signatures are
-/// fixed.
-static bool shouldRemoveReturns(const Function &F) {
-  return !F.arg_empty() && !F.isIntrinsic();
+void rewriteReturns(Function &F, std::vector<ReturnInst *> &Rets) {
 }
 
 /// Removes out-of-chunk arguments from functions, and modifies their calls
 /// accordingly. It also removes allocations of out-of-chunk arguments.
 static void extractReturnsFromModule(Oracle &O, ReducerWorkItem &WorkItem) {
   Module &Program = WorkItem.getModule();
-  std::vector<Argument *> InitArgsToKeep;
-  std::vector<Function *> Funcs;
-  // Get inside-chunk arguments, as well as their parent function
-  for (auto &F : Program)
+  for (auto &F : Program) {
     if (shouldRemoveReturns(F)) {
-      Funcs.push_back(&F);
-      for (auto &A : F.args())
-        if (O.shouldKeep())
-          InitArgsToKeep.push_back(&A);
-    }
-
-  // We create a vector first, then convert it to a set, so that we don't have
-  // to pay the cost of rebalancing the set frequently if the order we insert
-  // the elements doesn't match the order they should appear inside the set.
-  std::set<Argument *> ArgsToKeep(InitArgsToKeep.begin(), InitArgsToKeep.end());
-
-  for (auto *F : Funcs) {
-    ValueToValueMapTy VMap;
-    std::vector<WeakVH> InstToDelete;
-    for (auto &A : F->args())
-      if (!ArgsToKeep.count(&A)) {
-        // By adding undesired arguments to the VMap, CloneFunction will remove
-        // them from the resulting Function
-        VMap[&A] = getDefaultValue(A.getType());
-        for (auto *U : A.users())
-          if (auto *I = dyn_cast<Instruction>(*&U))
-            InstToDelete.push_back(I);
+      std::vector<ReturnInst *> RetsToRewrite;
+      for (auto &BB : F) {
+        for (auto &I : BB) {
+          auto RI = dyn_cast<ReturnInst>(&I);
+          if (RI && !O.shouldKeep())
+            RetsToRewrite.push_back(RI);
+        }
       }
-    // Delete any (unique) instruction that uses the argument
-    for (Value *V : InstToDelete) {
-      if (!V)
-        continue;
-      auto *I = cast<Instruction>(V);
-      I->replaceAllUsesWith(getDefaultValue(I->getType()));
-      if (!I->isTerminator())
-        I->eraseFromParent();
+      if (!RetsToRewrite.empty())
+        rewriteReturns(F, RetsToRewrite);
     }
-
-    // No arguments to reduce
-    if (VMap.empty())
-      continue;
-
-    std::set<int> ArgIndexesToKeep;
-    for (const auto &[Index, Arg] : enumerate(F->args()))
-      if (ArgsToKeep.count(&Arg))
-        ArgIndexesToKeep.insert(Index);
-
-    auto *ClonedFunc = CloneFunction(F, VMap);
-    // In order to preserve function order, we move Clone after old Function
-    ClonedFunc->removeFromParent();
-    Program.getFunctionList().insertAfter(F->getIterator(), ClonedFunc);
-
-    replaceFunctionCalls(*F, *ClonedFunc, ArgIndexesToKeep);
-    // Rename Cloned Function to Old's name
-    std::string FName = std::string(F->getName());
-    F->replaceAllUsesWith(ConstantExpr::getBitCast(ClonedFunc, F->getType()));
-    F->eraseFromParent();
-    ClonedFunc->setName(FName);
   }
 }
 
