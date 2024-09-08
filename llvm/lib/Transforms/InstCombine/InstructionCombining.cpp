57ecd3a09f6bc2fdb8b4feaa0c74e47e16e05471
Lee <lee10202013@gmail.com>
2024-09-07 19:16:37 -0600

    Add tests for 3 inputs optimization

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -5080,7 +5080,7 @@ Instruction* cs6475_optimizer(Instruction *I) {
         auto INT_SMIN_ADD_ONE = APInt::getSignedMinValue(C->getUniqueInteger().getBitWidth()) + 1;
         if (INT_SMIN_ADD_ONE == C->getValue()) {
           if (match(B, m_SExt(m_Value(X))) && X && X->getType()->isIntegerTy() && X->getType()->getIntegerBitWidth() > 2) {
-	          log_optzn("Lee Wei");
+            log_optzn("Lee Wei");
             Type* InputType = X->getType();
             BasicBlock *BB = I->getParent();
             IRBuilder<> Builder(BB);
@@ -5116,7 +5116,7 @@ Instruction* cs6475_optimizer(Instruction *I) {
                   if (CallInst *CI = dyn_cast<CallInst>(UI.getUser())) {
                     Function *Callee = CI->getCalledFunction();
                     if (Callee && Callee->isIntrinsic() && (Callee->getIntrinsicID() == Intrinsic::assume)) {
-	                    log_optzn("Lee Wei");
+                      log_optzn("Lee Wei");
                       CI->eraseFromParent();
                       BasicBlock *BB = I->getParent();
                       IRBuilder<> Builder(BB);
